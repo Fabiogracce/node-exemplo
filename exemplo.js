d49e2f3cf098e0ce39927796f1e04000ed09ac9b
FABIO BUGANZA GRACCE <fabio.bgracce@senacsp.edu.br>
2023-11-08 14:56:40 -0300

    configuracao do ts

---
+++ exemplo.js
@@ -0,0 +1,21 @@
+
+/*let nome = "joao"// string
+let nome = 22 // int ou namber
+let altura = 1.90 // float,double, number
+let temGato = true//boolean
+
+let n1 = 10
+let n1 = 20
+
+const pi = 3.14
+
+*/
+/*
+let n1 = 20
+let n2 = 30
+let resultado = n1 + n2
+console.log(resultado)
+
+*/
+
+
